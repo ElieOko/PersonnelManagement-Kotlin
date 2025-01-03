53d69a30799c77ef566b9b4b7ddc01d16ba0ac54
ElieOko <elieoko100@gmail.com>
2025-01-03 18:19:38 +0100

    add:implementation depencies window && lifecycle with alias kotlin parcelize into plugins body

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -50,6 +51,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.window)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
