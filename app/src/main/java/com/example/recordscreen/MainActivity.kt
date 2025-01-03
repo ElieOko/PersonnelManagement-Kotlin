1475fb1369c5bbf5210e4dfd9b091b78d714e66b
ElieOko <elieoko100@gmail.com>
2025-01-03 19:06:15 +0100

    feat:add manager permission

--- app/src/main/java/com/example/recordscreen/MainActivity.kt
+++ app/src/main/java/com/example/recordscreen/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.recordscreen
 
+import android.os.Build
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.example.recordscreen.ui.theme.ColorGreen
+import com.example.recordscreen.ui.theme.ColorRed
 import com.example.recordscreen.ui.theme.RecordscreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +37,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecordscreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                var isServiceRunning by remember {
+                    mutableStateOf(false)
+                }
+                var hasNotificationPermission by remember {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        mutableStateOf(
+                            ContextCompat.checkSelfPermission(
+                                applicationContext,
+                                Manifest.permission.POST_NOTIFICATIONS
+                            ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    } else mutableStateOf(true)
+                }
 
+                val permissionLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) {
+                    isGaranted->
+                    if(hasNotificationPermission && !isServiceRunning){
+
+                    }
+                }
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Button(onClick = {
+                            if (!hasNotificationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                                permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                            } else{
+                                if (isServiceRunning){
+                                    //stop
+                                }
+                                else{
+                                    //start
+                                }
+                            }
+                        },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if (isServiceRunning) ColorRed else ColorGreen
+                            )
+
+                        ) {
+                            Text(text = if (isServiceRunning) "Stop recording" else "Start recording")
+                        }
+                    }
                 }
             }
         }
