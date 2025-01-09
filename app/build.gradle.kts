e65c5927c98f2249b3487dad50afe9bbeb68db45
ElieOko <elieoko100@gmail.com>
2025-01-09 11:37:17 +0100

    fix:other features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.window)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.window)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
